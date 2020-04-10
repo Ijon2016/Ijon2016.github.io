0fc7df5fca8a714f83349bfe46007f823907b8a1
yomori <yoichi.omori@gmail.com>
2020-04-10 10:22:14 +0900

    omit time

--- app.js
+++ app.js
@@ -3,18 +3,18 @@ let isPlaying = false
 //let tapCount, time = 0
 //const tapBtn    = document.getElementById('js-tapBtn')
 //const ADMedia  = document.getElementById('js-media').style.visibility = 'hidden'
-const JABtn  = document.getElementById('JABtn')
-const ENBtn  = document.getElementById('ENBtn')
+const JABtn = document.getElementById('JABtn')
+const ENBtn = document.getElementById('ENBtn')
 //const countText = document.getElementById('js-count')
-const timeText  = document.getElementById('js-time')
+const timeText = document.getElementById('js-time')
 
 // 再生時間の初期値設定
 const setGame = () => {
-    tapCount = 0
-    time = 10000
-    countText.innerText = tapCount
-    timeText.innerHTML = time / 1000
-  }
+  tapCount = 0
+  time = 10000
+  countText.innerText = tapCount
+  timeText.innerHTML = time / 1000
+}
 setGame()
 
 /*
@@ -28,24 +28,24 @@ tapBtn.addEventListener('click', () => {
 
 // 日本語ボタン
 JABtn.addEventListener('click', () => {
-//  setGame()
+  //  setGame()
   isPlaying = true
-//  tapBtn.disabled = false
+  //  tapBtn.disabled = false
   ENBtn.style.display = 'none'
 
   var v = document.getElementById("Jvideo")
   v.innerHTML = ""
   v.play()
 
-  const timer = setInterval( () => {
+  const timer = setInterval(() => {
     time -= 10
     timeText.innerHTML = (time / 1000).toFixed(2)
 
     if (time === 0) {
       clearInterval(timer)
-//      isPlaying = false
-//      JABtn.style.display = 'inline-block'
-//      JABtn.innerText = 'もう一回'
+      //      isPlaying = false
+      JABtn.style.display = 'inline-block'
+      //      JABtn.innerText = 'もう一回'
     }
   }, 10)
 })
@@ -53,11 +53,11 @@ JABtn.addEventListener('click', () => {
 // Englishボタン
 ENBtn.addEventListener('click', () => {
   //  setGame()
-    isPlaying = true
+  isPlaying = true
   //  tapBtn.disabled = false
-    JABtn.style.display = 'none'
-  
-    var v = document.getElementById("Jvideo")
-    v.play()
+  JABtn.style.display = 'none'
 
-  })
+  var v = document.getElementById("Jvideo")
+  v.play()
+
+})

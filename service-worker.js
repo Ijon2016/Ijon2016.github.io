c1ecf9c8f36eeb139e1f09928c10b5fb73db56f1
yomori <yoichi.omori@gmail.com>
2020-04-20 16:30:42 +0900

    add cache control

--- service-worker.js
+++ service-worker.js
@@ -9,3 +9,30 @@ self.addEventListener('activate', function(e) {
 
 // 現状では、この処理を書かないとService Workerが有効と判定されないようです
 self.addEventListener('fetch', function(event) {});
+
+// files to be cached
+var CACHE_NAME = 'pico-caches';
+var urlsToCache = [
+    'logo-152.png',
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+      caches
+          .open(CACHE_NAME)
+          .then(function(cache) {
+              return cache.addAll(urlsToCache);
+          })
+  );
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+      caches
+          .match(event.request)
+          .then(function(response) {
+              return response ? response : fetch(event.request);
+          })
+  );
+});
